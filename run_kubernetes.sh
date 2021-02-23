06eb273b49e81383524ba5447cdc9f7b62704879
krajeevs <krajeevs@yahoo.com>
2021-02-23 19:37:07 +0000

    first commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="krajeevs/myimage_v1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myimage --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward myimage 8000:80
