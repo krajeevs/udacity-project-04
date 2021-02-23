531fec88045f00b05134939ea327c638421daa4a
krajeevs <krajeevs@yahoo.com>
2021-02-23 18:34:57 +0000

    first commit

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=krajeevs/myimage_v1
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag myimage_v1 $dockerpath
+docker login -u krajeevs
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
